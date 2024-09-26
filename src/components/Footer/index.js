45bf6ce348d9ac52a82f214f24af9da272e073d7
Daniel Benatti <124791723+DanielBenatti@users.noreply.github.com>
2024-09-26 07:49:03 -0300

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2> EtecFlix &copy; Desenvolvido por Gabriel Caspirro Demarchi</h2>
+        </footer>
+    );
+}
+
+export default Footer;
